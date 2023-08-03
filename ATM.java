a74c053db17e41f18706f9e8d51a5d3bea4537ac
stan870224 <stan870224@gmail.com>
2023-08-03 21:15:30 +0800

    commit 20230803

--- ATM.java
+++ ATM.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ATM {
-    
+    private ArrayList<Account> accounts = new ArrayList<>();
+    private Scanner sc = new Scanner(System.in);
+
+    // start ATM system show the Welcome Page!!
+    public void start(){
+        while(true){
+            System.out.println("=========Welcome to the Happy Bank ATM=========");
+            System.out.println("1.Login Account" );
+            System.out.println("2.Create an account" );
+            System.out.println("Please choose a function");
+            String command = sc.next();
+        
+            switch(command){
+                case "1" :
+                    login(); // login account
+                    break;
+                case "2" :
+                    create(); // create account
+                    break;
+                default :
+                    System.out.println("We don't hava this function");
+                    System.out.println("Please choose again!!");
+            }
+        }
+    }
+
+    public void login(){
+
+    }
+
+    public void create(){
+
+    }
 }
